07bc4b4ebd3427519bd31ca576f7d41ea3bf9c91
Adams Okode <adamsokode@gmail.com>
2021-11-07 19:21:05 +0300

    package for prod

---
+++ server.js
@@ -0,0 +1,20 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
+// app.use(requireHTTPS);
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/quandl-viewer'));
+
+app.get('/*', (req,res,next) => {
+    res.sendFile(path.join(__dirname + '/dist/quandl-viewer/index.html'));
+});
+
+app.listen(process.env.PORT || 3001);
